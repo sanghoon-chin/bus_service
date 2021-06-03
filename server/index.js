d89cba7832226085cb59505fb2e9be31586200ec
sanghoon chin <sanghunjin1916@gmail.com>
2021-06-03 20:10:34 +0900

    fs 추가

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const fs = require('fs/promises');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('map.html', {
-        root: path.resolve(__dirname, '../frontend/dist')
-    })
+app.get('/', async (req, res) => {
+    const data = await fs.readFile(path.resolve(__dirname, '../frontend/dist/map.html'))
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
 })
+
+// app.get('/', (req, res) => {
+//     res.sendFile('map.html', {
+//         root: path.resolve(__dirname, '../frontend/dist')
+//     })
+// })
 
 const server = app.listen(80, () => {
     console.log('running')
